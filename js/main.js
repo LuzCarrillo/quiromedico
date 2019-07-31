9c5fc698e72d1ac068d4e89ed7c0cb31e6a9339c
Quiromedico <53538913+Quiromedico@users.noreply.github.com>
2019-07-31 15:35:12 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
-	debugger
+
 if (location.href.split("/").slice(-1)=="agenda-tu-cita.php" || location.href.split("/").slice(-1)=="ver-agenda-citas.php" ){
 	
 var tipoCitasApiString = "";
